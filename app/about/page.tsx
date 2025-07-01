078bb09bbb31c750140010eed5a1b6bdbdb9e6f1
Sebastian <sebastian.k.dittmann@gmail.com>
2025-07-01 18:37:49 +0200

    move repo link to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,20 @@
 'use client';
 
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 export default function AboutPage() {
     return (
         <Box className='flex flex-col items-center'>
             <Box className='max-w-screen-md w-full mx-auto'>
                 <Box className="flex items-center justify-center mt-20">
-                    <h1 className="text-2xl font-bold">About</h1>
+                    <Text className="text-2xl font-bold h1">About</Text>
                 </Box>
-                <Box className='mt-10'>
-                    <p className="text-center">I am a passionate software engineer from Denmark. I love experimenting with new
-                        concepts and technologies, and this is my space to experiment and learn.</p>
+                <Box className='mt-10 text-center'>
+                    <Text>I am a passionate software engineer from Denmark. I love experimenting with new
+                        concepts and technologies, and this is my space to experiment and learn.</Text>
+                    <Text className="mt-4">
+                        You can find the source code for this blog on <a href="https://github.com/sebastiankdittmann/homepage" className="dark:text-gray-400 text-amber-600 hover:text-blue-300">Github</a>!
+                    </Text>
                 </Box>
             </Box>
         </Box>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import TextLink from "@/components/textLink/TextLink";
-import { Box, List } from "@chakra-ui/react";
+import { Box, List, Text } from "@chakra-ui/react";
 import { StrictMode } from "react";
 
 const blogEntries = [
@@ -21,16 +21,13 @@ export default function Home() {
                 <Box className='flex flex-col items-center'>
                     <Box className='max-w-screen-md w-full mx-auto'>
                         <Box className="flex items-center justify-center mt-20">
-                            <h1 className="text-2xl font-bold">Welcome!</h1>
+                            <Text className="text-2xl font-bold">Welcome!</Text>
                         </Box>
                         <Box className='mt-10' justifyContent={'center'} display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                            <p className="text-center">
+                            <Text className="text-center">
                                 Welcome to my blog. Here I will share my experiences and learnings from my work as a
                                 software engineer. Maybe you will find some of it useful. Enjoy!
-                            </p>
-                            <p className="text-center">
-                                P.S.: You can find the source code for this blog on <a href="https://github.com/sebastiankdittmann/homepage" className="dark:text-gray-400 text-amber-600 hover:text-blue-300">Github</a>!
-                            </p>
+                            </Text>
                             <List.Root className="text-center pt-6">
                                 {blogEntries.sort((a, b) => a.publishDate.getTime() - b.publishDate.getTime()).map((entry, index) => (
                                     <List.Item key={index}>
